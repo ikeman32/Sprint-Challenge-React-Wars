486aedd4bf9d68200eecd65253bc3d1a23d53d72
ikeman32 <ikeman32@yaho.com>
2019-10-11 10:36:20 -0700

    Rendered with styles

--- starwars/src/components/People.js
+++ starwars/src/components/People.js
@@ -3,13 +3,15 @@ import axios from "axios";
 import { CharacterCard } from "./PeopleCard";
 
 export const Characters = props => {
-  const [char, setChar] = useState([]);
-  
+  const [charData, setCharData] = useState([]);
+  //const [characterCard, setCharacterCard]= useState([]);
+
   useEffect(() => {
     axios
       .get(`https://swapi.co/api/people/`)
       .then(res => {
         console.log(res.data.results);
+        setCharData(res.data.results);
       })
       .catch(error => {
         console.log("The data was not returned", error);
@@ -18,7 +20,17 @@ export const Characters = props => {
 
   return (
     <>
-      <CharacterCard />
+      {charData.map((char, index)=>(
+          <CharacterCard
+            key = {index} 
+            name = {char.name}
+            gender = {char.gender}
+            height = {char.height}
+            weight = {char.mass}
+            hair = {char.hair_color}
+            skin = {char.skin_color}
+            />
+      ))}
     </>
   );
 };
--- starwars/src/components/PeopleCard.js
+++ starwars/src/components/PeopleCard.js
@@ -1,11 +1,42 @@
 import React from "react";
-import styled form "styled-components";
+import styled from "styled-components";
+
+const SwPeople = styled.div`
+border: 1px solid red;
+background-color: none;
+width: 30%;
+margin: 0 auto;
+margin-bottom: 1rem;
+border-radius: 1rem;
+`;
+
+const Name = styled.h2`
+color: crimson;
+`;
+
+const Bio = styled.ul`
+list-style-type: none;
+`;
+
+const Info = styled.li`
+text-align: left;
+color: #730000;
+padding: 1rem;
+`;
 
 export const CharacterCard = props =>{
 
     return(
-        <div className='Characters'>
-        
-        </div>
+        <SwPeople>
+            <Name>Name: {props.name}</Name>
+            <Bio>
+                <Info name="Gender">Gender: {props.gender}</Info>
+                <Info name="Height">Height: {props.height}</Info>
+                <Info name="Weight">Mass: {props.weight}</Info>
+                <Info name="Hair_Color">Hair: {props.hair}</Info>
+                <Info name="Skin_Color">Skin: {props.skin}</Info>
+            </Bio>
+
+        </SwPeople>
     )
 }
